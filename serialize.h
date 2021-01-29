d0c147ec6fb5920e77dd2fc7067233b52557275e
Roy Bellingan <roy@tech.techadsmedia.com>
2021-01-29 14:05:53 +0000

    file creation time is bugged

--- serialize.h
+++ serialize.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "filefunction.h"
 #include <QDataStream>
+#include <QDateTime>
+#include <QFileInfo>
+
 template <typename T>
 uint fileSerialize(QString fileName, const T& t) {
 	QFileXT file;
@@ -16,21 +19,30 @@ uint fileSerialize(QString fileName, const T& t) {
 }
 
 struct UnserializeResult {
-	qint64 size       = 0;
-	bool   fileExists = false;
+	qint64    size       = 0;
+	bool      fileExists = false;
+	QDateTime age;
+	bool      valid = false;
 };
 template <typename T>
-UnserializeResult fileUnSerialize(QString fileName, T& t) {
+UnserializeResult fileUnSerialize(QString fileName, T& t, uint maxAge = 0) {
 
 	QFileXT file;
 	file.setFileName(fileName);
 	if (!file.open(QIODevice::ReadOnly, true)) {
-		return {0, false};
+		return {0, false, QDateTime()};
+	}
+
+	//AFAIK birtTime is a bit broken in linux http://moiseevigor.github.io/software/2015/01/30/get-file-creation-time-on-linux-with-ext4/
+	auto lastEdit = QFileInfo(file).metadataChangeTime();
+	//is the file fresh enought ?
+	if (lastEdit.toSecsSinceEpoch() < QDateTime::currentSecsSinceEpoch() - maxAge) {
+		return {0, true, QDateTime(), false};
 	}
 
 	QDataStream in(&file);
 	in.setVersion(QDataStream::Qt_DefaultCompiledVersion);
 
 	in >> t;
-	return {file.size(), true};
+	return {file.size(), true, lastEdit, true};
 }
