017ee8a78bc6fa53d11a626d2fae53f5f8b1c830
Roy Bellingan <roy@tech.techadsmedia.com>
2021-11-17 23:16:14 +0000

    APCU V1

--- apcu2.cpp
+++ apcu2.cpp
@@ -1,45 +1,26 @@
 #include "apcu2.h"
-
 using namespace std;
 
-
-
 //only used internally
-void APCU::throwTypeError(const type_info *found, const type_info *expected) {
+void APCU::throwTypeError(const type_info* found, const type_info* expected) {
 	throw ExceptionV2(QSL("Wrong type!! Found %1, expected %2, recheck where this key is used, maybe you have a collision").arg(found->name()).arg(expected->name()));
 }
 
 bool APCU::exists(const QString& key, bool lock, CacheType::iterator& iter) {
-	if (lock) {
-		std::lock_guard<std::mutex> scoped(innerLock);
-	}
-	
+	LockGuardV2 scoped(&innerLock, lock);
+
 	if (iter = cache.find(key); iter != cache.end()) {
 		if (iter->second.expired()) {
 			cache.erase(iter);
 			return false;
-		} else {
-			return true;
 		}
-	} else {
-		return false;
+		return true;
 	}
+	return false;
 }
 
 //public one
 bool APCU::exists(const QString& key, bool lock) {
 	CacheType::iterator iter;
 	return exists(key, lock, iter);
-}
-
-bool APCU::tryStore(const QString& key, std::any& obj, int ttl) {
-	//do not call apcuExists to avoid useless double lock unlock
-	std::lock_guard<std::mutex> scoped(innerLock);
-	if (exists(key, false)) {
-		return true;
-	} else {
-		auto copy  = make_shared<any>(obj);
-		cache[key] = Value(copy, ttl);
-		return false;
-	}
 }
--- apcu2.h
+++ apcu2.h
@@ -2,6 +2,7 @@
 
 #include "QStacker/exceptionv2.h"
 #include "mapExtensor/hmap.h"
+#include "mapExtensor/lockguardv2.h"
 #include <QDateTime>
 #include <QHash>
 #include <QString>
@@ -25,7 +26,7 @@ class NonCopyable {
 
 class APCU : private NonCopyable {
       public:
-	APCU* get() {
+	static APCU* get() {
 		static APCU* instance = nullptr;
 		if (!instance) {
 			instance = new APCU();
@@ -33,37 +34,7 @@ class APCU : private NonCopyable {
 		return instance;
 	}
 
-      private:
-	std::mutex innerLock;
-
-	void throwTypeError(const std::type_info* found, const std::type_info* expected);
-	struct Value {
-		shared_ptr<void*> obj;
-		//I could have used std::any but at this point is just cumbersome
-		const std::type_info* type     = nullptr;
-		int                   expireAt = 0;
-		Value()                        = delete;
-
-		template <class T>
-		Value(shared_ptr<T>& obj, int ttl) {
-			this->obj = obj;
-			type      = &typeid(T);
-			expireAt  = QDateTime::currentSecsSinceEpoch() + ttl;
-		}
-		bool expired() const {
-			return QDateTime::currentSecsSinceEpoch() > expireAt;
-		}
-		template <class T>
-		bool isSameType(T) const {
-			return type->hash_code() == typeid(T).hash_code();
-		}
-	};
-
-	using CacheType = hmap<QString, Value>;
-	CacheType cache;
-
-	bool exists(const QString& key, bool lock, CacheType::iterator& iter);
-	bool exists(const QString& key, bool lock);
+	bool exists(const QString& key, bool lock = true);
 
 	/**
 	 * @brief apcuStore will OVERWRITE IF IS FOUND
@@ -72,19 +43,81 @@ class APCU : private NonCopyable {
 	 * @param ttl
 	 */
 	template <class T>
-	void store(const QString& key, T& obj, int ttl = 60) {
-		auto                        copy = make_shared<any>(obj);
-		std::lock_guard<std::mutex> scoped(innerLock);
+	void store(const QString& key, T& obj, int ttl = 60, bool lock = true) {
+		auto copy = make_shared<T>(obj);
+		store(key, copy, ttl, lock);
+	}
+
+	//If you pass a shared ptr, it will not deep copy it
+	//passing a * is wild and will not be tollerated instead
+	template <class T>
+	void store(const QString& key, shared_ptr<T>& obj, int ttl = 60, bool lock = true) {
+		LockGuardV2 scoped(&innerLock, lock);
+
 		if (auto iter = cache.find(key); iter != cache.end()) {
-			auto& ref = iter->second;
-			ref.type  = &typeid(T);
-			ref.obj.reset();
-			ref.obj      = copy;
-			ref.expireAt = QDateTime::currentSecsSinceEpoch() + ttl;
+			iter->second = Value(obj, ttl);
 		} else {
-			cache[key] = {copy, ttl};
+			cache[key] = {obj, ttl};
 		}
 	}
+
+	template <class T>
+	std::shared_ptr<T> apcuFetch(const QString& key) {
+		CacheType::iterator iter;
+		if (exists(key, false, iter)) {
+			auto& ref = iter->second;
+			if (ref.isSameType<T>()) {
+				return static_pointer_cast<T>(ref.obj);
+			} else {
+				throwTypeError(ref.type, &typeid(T));
+			}
+		} else {
+			return nullptr;
+		}
+	}
+
+      private:
+	std::mutex innerLock;
+
+	void throwTypeError(const std::type_info* found, const std::type_info* expected);
+	struct Value {
+		shared_ptr<void> obj = nullptr;
+		//I could have used std::any but at this point is just cumbersome
+		//https://en.cppreference.com/w/cpp/language/typeid
+		const std::type_info* type     = nullptr;
+		int                   expireAt = 0;
+		//Value()                        = delete;
+		Value() {
+			int x = 0;
+		}
+		template <class T>
+		Value(shared_ptr<T>& obj, int ttl) {
+			this->obj = (shared_ptr<void>)obj;
+			type      = &typeid(T);
+			expireAt  = QDateTime::currentSecsSinceEpoch() + ttl;
+		}
+		template <class T>
+		Value(T& obj, int ttl) {
+			auto             copy = make_shared<T>(obj);
+			shared_ptr<void> obj2 = (shared_ptr<void>)copy;
+			this->obj             = obj2;
+			type                  = &typeid(T);
+			expireAt              = QDateTime::currentSecsSinceEpoch() + ttl;
+		}
+		bool expired() const {
+			return QDateTime::currentSecsSinceEpoch() > expireAt;
+		}
+		template <class T>
+		bool isSameType() const {
+			return type->hash_code() == typeid(T).hash_code();
+		}
+	};
+
+	using CacheType = hmap<QString, Value>;
+	CacheType cache;
+
+	bool exists(const QString& key, bool lock, CacheType::iterator& iter);
+
 	/**
 	 * @brief apcuTryStore
 	 * @param key
@@ -92,23 +125,15 @@ class APCU : private NonCopyable {
 	 * @param ttl
 	 * @return if we inserted (there was NONE or EXPIRED) or not
 	 */
-	bool tryStore(const QString& key, std::any& obj, int ttl = 60);
-
-	QString apcuFetch(const QString& key);
-
 	template <class T>
-	std::shared_ptr<T> apcuFetch(const QString& key) {
-		CacheType::iterator iter;
+	bool tryStore(const QString& key, T& obj, int ttl = 60) {
+		CacheType::iterator         iter;
+		std::lock_guard<std::mutex> scoped(innerLock);
 		if (exists(key, false, iter)) {
-			auto& ref = iter->second;
-			//https://en.cppreference.com/w/cpp/language/typeid
-			if (ref.isSameType<T>()) {
-				return (shared_ptr<T>)ref.obj;
-			} else {
-				throwTypeError(ref.type, &typeid(T));
-			}
+			return true;
 		} else {
-			return nullptr;
+			iter->second = Value(obj, ttl);
+			return false;
 		}
 	}
 };
